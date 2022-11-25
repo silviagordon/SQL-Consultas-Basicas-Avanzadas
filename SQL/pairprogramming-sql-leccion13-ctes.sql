e8decbefc2b01b7b03d57e3499cd108c90c613ef
solkiria <sonia.ruiz.p31@gmail.com>
2022-11-25 10:47:22 +0100

    subir ejers sql CTEs

---
+++ SQL/pairprogramming-sql-leccion13-ctes.sql
@@ -0,0 +1,28 @@
+/*
+Pair Programming - Leccion 13
+Lourdes Ochoa, Silvia Gordón y Sonia Ruiz.
+Hoy praticaremos las CTE's.
+
+1. Extraer en una CTE todos los nombres de las compañias y los id de los clientes.
+Para empezar nos han mandado hacer una CTE muy sencilla el id del cliente y el nombre de la compañia de la tabla Customers.
+Los resultados de esta query tendrán las columnas CustID y CompanyName.*/
+
+/*2. Selecciona solo los de que vengan de "Germany"
+Ampliemos un poco la query anterior. En este caso, queremos un resultado similar al anterior, pero solo queremos los que pertezcan a "Germany".
+Los resultados de esta query tendrán las columnas CustID y CompanyName.*/
+
+/*3. Extraed el id de las facturas y su fecha de cada cliente.
+En este caso queremos extraer todas las facturas que se han emitido a un cliente, su fecha la compañia a la que pertenece.
+📌 NOTA En este caso tendremos columnas con elementos repetidos(CustomerID, y Company Name).
+Los resultados de esta query tendrán las columnas cutomer_id, company_name, order_id, oder_date.*/
+
+/*4. Contad el número de facturas por cliente
+Mejoremos la query anterior. En este caso queremos saber el número de facturas emitidas por cada cliente.
+Los resultados de esta query tendrán las columnas cutomer_id, company_name, numero_facturas.*/
+
+/*5. Cuál la cantidad media pedida de todos los productos ProductID.
+Necesitaréis extraer la suma de las cantidades por cada producto y calcular la media.
+Los resultados de esta query tendrán las columnas producto, media_p?:
+
+Happy coding 💪🏽
+*/
