a0c6d5f02581e3a682b6a4795fa980419d907385
Silvia Gordón <115147263+silviagordon@users.noreply.github.com>
2023-02-15 10:18:07 +0100

    Update and rename pairprogramming-sql-leccion13-ctes.sql to ctes.sql

--- SQL/ctes.sql
+++ SQL/ctes.sql
@@ -1,6 +1,3 @@
--- Ejercicios Pair programming Sprint 2
--- Lección 13: CTEs
--- Silvia Gordón, Lourdes Ochoa, Sonia Ruíz
 
 /* 1. Extraer en una CTE todos los nombres de las compañias y los id de los clientes.
 Para empezar nos han mandado hacer una CTE muy sencilla el id del cliente y el nombre de la compañia de la tabla Customers.
